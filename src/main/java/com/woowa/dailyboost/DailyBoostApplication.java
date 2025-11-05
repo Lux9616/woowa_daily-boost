ddaa041805c461ff0aaf7be224169d11479b7078
dawn <shincu7@gmail.com>
2025-11-06 04:02:21 +0900

    feat: initial Spring Boot project setup

---
+++ src/main/java/com/woowa/dailyboost/DailyBoostApplication.java
@@ -0,0 +1,13 @@
+package com.woowa.dailyboost;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyBoostApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyBoostApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/woowa/dailyboost/DailyBoostApplicationTests.java
@@ -0,0 +1,13 @@
+package com.woowa.dailyboost;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DailyBoostApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
